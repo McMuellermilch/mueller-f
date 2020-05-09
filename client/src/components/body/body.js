32ab0989db851bee06d98b2b490789750f0c26f9
Florian <mueller.florian99@outlook.com>
2020-05-09 23:07:52 +0200

    basic layout to client added

---
+++ client/src/components/body/body.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './body.css';
+
+const Body = () => {
+  return <div>Body component</div>;
+};
+
+export default Body;
---
+++ client/src/components/heading/heading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './heading.css';
+
+const Heading = () => {
+  return <div className="heading">heading component</div>;
+};
+
+export default Heading;
