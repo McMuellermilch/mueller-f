24a1663761e800cf6db194dea6156a17f4fda1d0
Florian <mueller.florian99@outlook.com>
2020-05-14 11:55:35 +0200

    added modal and made project card clickable

--- client/src/components/body/projects/project/project.js
+++ client/src/components/body/projects/project/project.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './project.css';
 
-import { Card, Icon, Label } from 'semantic-ui-react';
+import { Card, Modal, Label, Header, Image } from 'semantic-ui-react';
 import 'react-circular-progressbar/dist/styles.css';
 
 const Project = (props) => {
+  const [visible, setVisible] = useState(false);
   const colors = {
     JavaScript: 'blue',
     Java: 'brown',
@@ -16,7 +17,7 @@ const Project = (props) => {
 
   return (
     <div className="project">
-      <Card>
+      <Card onClick={() => setVisible(true)}>
         <Card.Content>
           <Card.Header>{props.title}</Card.Header>
           <Card.Meta>
@@ -34,6 +35,25 @@ const Project = (props) => {
         </Card.Content>
         <Card.Content extra></Card.Content>
       </Card>
+
+      <Modal
+        open={visible}
+        closeOnEscape={() => setVisible(false)}
+        onClose={() => setVisible(false)}
+      >
+        <Modal.Header>Select a Photo</Modal.Header>
+        <Modal.Content image>
+          <Image wrapped size="medium" src="/images/avatar/large/rachel.png" />
+          <Modal.Description>
+            <Header>Default Profile Image</Header>
+            <p>
+              We've found the following gravatar image associated with your
+              e-mail address.
+            </p>
+            <p>Is it okay to use this photo?</p>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
     </div>
   );
 };
