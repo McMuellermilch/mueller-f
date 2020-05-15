62e1a7dc5e737039d6b464b93e4626133f586e60
Florian <mueller.florian99@outlook.com>
2020-05-15 23:08:20 +0200

    modal added to the email button in the header

--- client/src/components/heading/heading.js
+++ client/src/components/heading/heading.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './heading.css';
 
-import { Button } from 'semantic-ui-react';
+import { Button, Modal, Form, TextArea } from 'semantic-ui-react';
 import 'react-circular-progressbar/dist/styles.css';
 
 const Heading = () => {
+  const [anrede, setAnrede] = useState('');
+  const [vorname, setVorname] = useState('');
+  const [nachname, setNachname] = useState('');
+  const [email, setEmail] = useState('');
+  const [nachricht, setNachricht] = useState('');
+
   return (
     <div className="heading">
       <div className="heading_title">mueller f.</div>
-      <div className="heading_mail" onClick={() => console.log('clicked')}>
-        <Button circular color="#2185D0" icon="mail outline" />
+      <div className="heading_mail">
+        <Modal trigger={<Button circular color="blue" icon="mail outline" />}>
+          <Modal.Header>Schicken Sie mir 'ne E-Mail!</Modal.Header>
+          <Modal.Content>
+            <Form>
+              <Form.Group widths="equal">
+                <Form.Input
+                  fluid
+                  placeholder="Anrede"
+                  onChange={(event) => setAnrede(event.target.value)}
+                />
+                <Form.Input
+                  fluid
+                  placeholder="Vorname"
+                  onChange={(event) => setVorname(event.target.value)}
+                />
+                <Form.Input
+                  fluid
+                  placeholder="Nachname"
+                  onChange={(event) => setNachname(event.target.value)}
+                />
+              </Form.Group>
+              <Form.Input
+                placeholder="E-Mail"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <TextArea
+                placeholder="Nachricht"
+                onChange={(event) => setNachricht(event.target.value)}
+              />
+            </Form>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button negative>Abbrechen</Button>
+            <Button positive>Senden</Button>
+          </Modal.Actions>
+        </Modal>
       </div>
     </div>
   );
