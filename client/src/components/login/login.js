94a60699edddffb65a139b36e294ee09bef898a0
Florian <mueller.florian99@outlook.com>
2020-05-20 21:29:30 +0200

    added styling and content to ManagementLongin page

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
 
+import { Input, Button } from 'semantic-ui-react';
+import 'react-circular-progressbar/dist/styles.css';
+
 const Login = () => {
-  return <div>login route</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="login">
+      <div className="login_content">
+        <div className="login_title">Management Console Login</div>
+        <div className="login_body">
+          <div className="login_body_input">
+            <Input
+              fluid
+              placeholder="Username..."
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div className="login_body_input">
+            <Input
+              fluid
+              placeholder="Passwort..."
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <Button fluid color="blue">
+            Login
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
