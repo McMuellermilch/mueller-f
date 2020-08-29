8e6725a6a8dff66194558215c3e8fbccb766ab36
Florian <mueller.florian99@outlook.com>
2020-08-29 15:58:29 +0200

    added the color attribute throughout all components to the labels

--- client/src/components/body/projects/project/project.js
+++ client/src/components/body/projects/project/project.js
@@ -6,14 +6,24 @@ import 'react-circular-progressbar/dist/styles.css';
 
 const Project = (props) => {
   const [visible, setVisible] = useState(false);
-  const colors = {
-    JavaScript: 'blue',
-    Java: 'brown',
-    Golang: 'teal',
-    Python: 'purple',
-    VisualBasic: 'green',
-    Dart: 'red',
+
+  const getColor = (name) => {
+    let item = props.skills.find((elem, index) => {
+      if (elem.name == name) {
+        return elem.color;
+      }
+    });
+    return item;
   };
+
+  function getContrastYIQ(hexcolor) {
+    hexcolor = hexcolor.replace('#', '');
+    var r = parseInt(hexcolor.substr(0, 2), 16);
+    var g = parseInt(hexcolor.substr(2, 2), 16);
+    var b = parseInt(hexcolor.substr(4, 2), 16);
+    var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+    return yiq >= 128 ? 'black' : 'white';
+  }
 
   return (
     <div className="project">
@@ -23,11 +33,20 @@ const Project = (props) => {
           <Card.Meta>
             <div className="project_label">
               {props.labelText.map((language, index) => {
+                let item = getColor(language);
+                let color = '#222f3e';
+                if (item != undefined) {
+                  color = item.color;
+                }
+
                 return (
                   <Label
                     key={index}
                     size="tiny"
-                    style={{ backgroundColor: props.labelColor }}
+                    style={{
+                      backgroundColor: color,
+                      color: getContrastYIQ(color),
+                    }}
                   >
                     {language}
                   </Label>
--- client/src/components/body/projects/projects.js
+++ client/src/components/body/projects/projects.js
@@ -4,45 +4,25 @@ import './projects.css';
 import Project from './project/project';
 
 const Projects = (props) => {
-  const [projects, setProjects] = useState([]);
-  const [skills, setSkills] = useState([]);
-
-  useEffect(() => {
-    if (projects.length == 0 && props.projectData.length != 0) {
-      setProjects(
-        props.projectData.map((project, index) => {
+  return (
+    <div className="projects">
+      <div className="projects_box_heading">Projects</div>
+      <div className="projects_box_body">
+        {props.projectData.map((project, index) => {
           return (
             <Project
               key={index}
               title={project.name}
               labelText={project.languages}
-              lableColor="#ff6b6b"
               descriptionShort={project.descriptionShort}
               descriptionLong={project.descriptionLong}
               tags={project.tags}
               gitHubLink={project.gitHubLink}
+              skills={props.skillData}
             />
           );
-        })
-      );
-    }
-    if (skills.length == 0 && props.skillData.length != 0) {
-      setSkills(
-        props.skillData.map((skill, index) => {
-          return {
-            index: index,
-            name: skill.name,
-            color: skill.color,
-          };
-        })
-      );
-    }
-  });
-
-  return (
-    <div className="projects">
-      <div className="projects_box_heading">Projects</div>
-      <div className="projects_box_body">{projects}</div>
+        })}
+      </div>
     </div>
   );
 };
